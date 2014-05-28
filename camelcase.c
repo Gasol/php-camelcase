1e3037c2008a0d955078ee8b53b42f6e87f12ff5
Gasol Wu <gasol.wu@gmail.com>
2014-05-28 23:33:13 +0800

    Some cleanup

--- camelcase.c
+++ camelcase.c
@@ -12,11 +12,9 @@
   | obtain it through the world-wide-web, please send a note to          |
   | license@php.net so we can mail you a copy immediately.               |
   +----------------------------------------------------------------------+
-  | Author:                                                              |
+  | Author: Gasol Wu <gasol.wu@gmail.com>                                |
   +----------------------------------------------------------------------+
 */
-
-/* $Id$ */
 
 #ifdef HAVE_CONFIG_H
 #include "config.h"
@@ -27,20 +25,14 @@
 #include "ext/standard/info.h"
 #include "php_camelcase.h"
 
-/* If you declare any globals in php_camelcase.h uncomment this:
-ZEND_DECLARE_MODULE_GLOBALS(camelcase)
-*/
-
 /* True global resources - no need for thread safety here */
 static int le_camelcase;
 
 /* {{{ camelcase_functions[]
- *
- * Every user visible function must have an entry in camelcase_functions[].
  */
 const zend_function_entry camelcase_functions[] = {
-	PHP_FE(confirm_camelcase_compiled,	NULL)		/* For testing, remove later. */
-	PHP_FE_END	/* Must be the last line in camelcase_functions[] */
+	PHP_FE(camelize,	NULL)
+	PHP_FE_END
 };
 /* }}} */
 
@@ -54,8 +46,8 @@ zend_module_entry camelcase_module_entry = {
 	camelcase_functions,
 	PHP_MINIT(camelcase),
 	PHP_MSHUTDOWN(camelcase),
-	PHP_RINIT(camelcase),		/* Replace with NULL if there's nothing to do at request start */
-	PHP_RSHUTDOWN(camelcase),	/* Replace with NULL if there's nothing to do at request end */
+	NULL,
+	NULL,
 	PHP_MINFO(camelcase),
 #if ZEND_MODULE_API_NO >= 20010901
 	PHP_CAMELCASE_VERSION,
@@ -68,34 +60,10 @@ zend_module_entry camelcase_module_entry = {
 ZEND_GET_MODULE(camelcase)
 #endif
 
-/* {{{ PHP_INI
- */
-/* Remove comments and fill if you need to have entries in php.ini
-PHP_INI_BEGIN()
-    STD_PHP_INI_ENTRY("camelcase.global_value",      "42", PHP_INI_ALL, OnUpdateLong, global_value, zend_camelcase_globals, camelcase_globals)
-    STD_PHP_INI_ENTRY("camelcase.global_string", "foobar", PHP_INI_ALL, OnUpdateString, global_string, zend_camelcase_globals, camelcase_globals)
-PHP_INI_END()
-*/
-/* }}} */
-
-/* {{{ php_camelcase_init_globals
- */
-/* Uncomment this function if you have INI entries
-static void php_camelcase_init_globals(zend_camelcase_globals *camelcase_globals)
-{
-	camelcase_globals->global_value = 0;
-	camelcase_globals->global_string = NULL;
-}
-*/
-/* }}} */
-
 /* {{{ PHP_MINIT_FUNCTION
  */
 PHP_MINIT_FUNCTION(camelcase)
 {
-	/* If you have INI entries, uncomment these lines 
-	REGISTER_INI_ENTRIES();
-	*/
 	return SUCCESS;
 }
 /* }}} */
@@ -103,27 +71,6 @@ PHP_MINIT_FUNCTION(camelcase)
 /* {{{ PHP_MSHUTDOWN_FUNCTION
  */
 PHP_MSHUTDOWN_FUNCTION(camelcase)
-{
-	/* uncomment this line if you have INI entries
-	UNREGISTER_INI_ENTRIES();
-	*/
-	return SUCCESS;
-}
-/* }}} */
-
-/* Remove if there's nothing to do at request start */
-/* {{{ PHP_RINIT_FUNCTION
- */
-PHP_RINIT_FUNCTION(camelcase)
-{
-	return SUCCESS;
-}
-/* }}} */
-
-/* Remove if there's nothing to do at request end */
-/* {{{ PHP_RSHUTDOWN_FUNCTION
- */
-PHP_RSHUTDOWN_FUNCTION(camelcase)
 {
 	return SUCCESS;
 }
@@ -136,22 +83,11 @@ PHP_MINFO_FUNCTION(camelcase)
 	php_info_print_table_start();
 	php_info_print_table_header(2, "camelcase support", "enabled");
 	php_info_print_table_end();
-
-	/* Remove comments if you have entries in php.ini
-	DISPLAY_INI_ENTRIES();
-	*/
 }
 /* }}} */
 
 
-/* Remove the following function when you have successfully modified config.m4
-   so that your module can be compiled into PHP, it exists only for testing
-   purposes. */
-
-/* Every user-visible function in PHP should document itself in the source */
-/* {{{ proto string confirm_camelcase_compiled(string arg)
-   Return a string to confirm that the module is compiled in */
-PHP_FUNCTION(confirm_camelcase_compiled)
+PHP_FUNCTION(camelize)
 {
 	char *arg = NULL;
 	int arg_len, len;
@@ -160,17 +96,7 @@ PHP_FUNCTION(confirm_camelcase_compiled)
 	if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "s", &arg, &arg_len) == FAILURE) {
 		return;
 	}
-
-	len = spprintf(&strg, 0, "Congratulations! You have successfully modified ext/%.78s/config.m4. Module %.78s is now compiled into PHP.", "camelcase", arg);
-	RETURN_STRINGL(strg, len, 0);
 }
-/* }}} */
-/* The previous line is meant for vim and emacs, so it can correctly fold and 
-   unfold functions in source code. See the corresponding marks just before 
-   function definition, where the functions purpose is also documented. Please 
-   follow this convention for the convenience of others editing your code.
-*/
-
 
 /*
  * Local variables:
--- php_camelcase.h
+++ php_camelcase.h
@@ -24,7 +24,7 @@
 extern zend_module_entry camelcase_module_entry;
 #define phpext_camelcase_ptr &camelcase_module_entry
 
-#define PHP_CAMELCASE_VERSION "0.1.0" /* Replace with version number for your extension */
+#define PHP_CAMELCASE_VERSION "0.1.0"
 
 #ifdef PHP_WIN32
 #	define PHP_CAMELCASE_API __declspec(dllexport)
@@ -40,31 +40,9 @@ extern zend_module_entry camelcase_module_entry;
 
 PHP_MINIT_FUNCTION(camelcase);
 PHP_MSHUTDOWN_FUNCTION(camelcase);
-PHP_RINIT_FUNCTION(camelcase);
-PHP_RSHUTDOWN_FUNCTION(camelcase);
 PHP_MINFO_FUNCTION(camelcase);
 
-PHP_FUNCTION(confirm_camelcase_compiled);	/* For testing, remove later. */
-
-/* 
-  	Declare any global variables you may need between the BEGIN
-	and END macros here:     
-
-ZEND_BEGIN_MODULE_GLOBALS(camelcase)
-	long  global_value;
-	char *global_string;
-ZEND_END_MODULE_GLOBALS(camelcase)
-*/
-
-/* In every utility function you add that needs to use variables 
-   in php_camelcase_globals, call TSRMLS_FETCH(); after declaring other 
-   variables used by that function, or better yet, pass in TSRMLS_CC
-   after the last function argument and declare your utility function
-   with TSRMLS_DC after the last declared argument.  Always refer to
-   the globals in your function as CAMELCASE_G(variable).  You are 
-   encouraged to rename these macros something shorter, see
-   examples in any other php module directory.
-*/
+PHP_FUNCTION(camelize);
 
 #ifdef ZTS
 #define CAMELCASE_G(v) TSRMG(camelcase_globals_id, zend_camelcase_globals *, v)
@@ -73,7 +51,6 @@ ZEND_END_MODULE_GLOBALS(camelcase)
 #endif
 
 #endif	/* PHP_CAMELCASE_H */
-
 
 /*
  * Local variables:
